d4112cbdda8e895c01afe3c17c21a7b806e0488b
Doug Winter <doug@isotoma.com>
2010-11-22 13:09:37 +0000

    working setup

--- setup.py
+++ setup.py
@@ -2,7 +2,16 @@ import os
 
 from setuptools import setup, find_packages
 setup(name='isotoma.plone.defang',
-      version="1.0",
+      version="1.0.1",
+      description="modify ZODBs for environment migration",
+      long_description = open("README.rst").read() + "\n" + \
+                         open("CHANGES.txt").read(),
+      classifiers = [
+        "Framework :: Buildout",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX",
+        "License :: OSI Approved :: Apache Software License",
+      ],
       packages=find_packages(),
       entry_points = {
         'console_scripts': ['defang=isotoma.plone.defang:main']
